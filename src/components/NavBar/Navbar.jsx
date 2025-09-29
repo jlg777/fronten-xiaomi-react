4509b99ba380fe1bf71bd0431efdb709fc3ddf7d
jlg777 <proyectojlg777@gmail.com>
2025-09-29 07:50:37 -0300

    update

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,51 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg header-nav">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"
-            ><i className="bi bi-phone-fill"></i>Digitalers Xiaomi
-          </a>
-          <button
-            className="navbar-toggler border-white"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#"
-                  >Principal
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Productos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Nosotros</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Admin</a>
-              </li>
-            </ul>
-          </div>
-          <button
-            id="themeToggleBtn"
-            className="btn btn-outline-light ms-3 btn-theme-toggle"
-            title="Cambiar tema"
-          >
-            <i className="bi bi-moon-fill"></i>
-          </button>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <i className="bi bi-phone-fill"></i> Digitalers Xiaomi
+        </Link>
+        <button
+          className="navbar-toggler border-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Principal
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/productos">
+                Productos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/nosotros">
+                Nosotros
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/admin">
+                Admin
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-  )
-}
+        <button
+          id="themeToggleBtn"
+          className="btn btn-outline-light ms-3 btn-theme-toggle"
+          title="Cambiar tema"
+        >
+          <i className="bi bi-moon-fill"></i>
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/NavBar/Navbar";
 import ProductForm from "../components/Products/ProductForm";
 import ProductSearch from "../components/Products/ProductSearch";
 import ProductTable from "../components/Products/ProductTable";
 import "../css/admin.css";
 import "../css/layout.css";
+import useProducts from "../hooks/useProducts";
 
 const Admin = () => {
+  const { products, loading, error } = useProducts();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  // Se actualiza cuando se cargan los productos por primera vez
+  useEffect(() => {
+    setFilteredProducts(products);
+    console.log(filteredProducts);
+  }, [products]);
+
   return (
     <>
+      <Navbar />
       <div className="layout">
         <main className="contenido container-xxxl">
           <h1 className="main-title text-center">ADMINISTRADOR DE PRODUCTOS</h1>
@@ -14,11 +27,16 @@ const Admin = () => {
             <ProductForm />
             <div className="col-12 col-lg-10">
               <ProductSearch />
-              <ProductTable />
+              <ProductTable
+                products={filteredProducts}
+                loading={loading}
+                error={error}
+              />
             </div>
           </div>
         </main>
       </div>
+      <Footer />
     </>
   );
 };
