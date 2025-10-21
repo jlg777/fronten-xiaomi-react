35e7ac4fe8b5b399c9a029ac6b4cd772dad16d67
Jorge Grandia <jgrandia@nucetemza.com>
2025-10-21 10:20:34 -0300

    ✨ Se cargan imagenes a cloudinary

--- src/components/Products/ProductImage.jsx
+++ src/components/Products/ProductImage.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
 
-export const ProductImage = ({ data }) => {
-  const [imageUrl, setImageUrl] = useState("");
+export const ProductImage = ({ onImageChange, initialImage = "" }) => {
+  const [imageUrl, setImageUrl] = useState(initialImage);
 
   const handleImageUpload = async (e) => {
     const file = e.target.files[0];
-
+if (!file) return;
     //console.log(file);
     try {
       const formData = new FormData();
@@ -16,9 +16,11 @@ export const ProductImage = ({ data }) => {
         "https://api.cloudinary.com/v1_1/dogs9oddm/image/upload",
         formData
       );
-      setImageUrl(result.data.secure_url);
-      console.log(result.data.secure_url);
-      return result.data.secure_url;
+      const url = result.data.secure_url;
+      setImageUrl(url);
+      onImageChange(url)
+      //console.log(result.data.secure_url);
+      return url;
     } catch (error) {
       console.log(error);
     }
