a7c8ea3321179640cab9d9165ec3d079bfd21b0c
Jorge Grandia <jgrandia@nucetemza.com>
2026-01-20 16:04:51 -0300

    pruvate route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const { isAuthenticated, loading } = useContext(AuthContext);
+  console.log(isAuthenticated)
   if (loading) return <div>Cargando...</div>;
 
   if (!isAuthenticated) return <Navigate to="/login" />;
